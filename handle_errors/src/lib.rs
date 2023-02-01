4835d47e5550678b821ed06436f22e7946044877
royrustdev <royrustdev@gmail.com>
2023-02-01 10:18:32 +0530

    release v1.0.0 error handling crate

--- handle_errors/src/lib.rs
+++ handle_errors/src/lib.rs
@@ -45,40 +45,26 @@ impl std::fmt::Display for APILayerError {
 impl std::fmt::Display for Error {
     fn fmt(&self, f: &mut std::fmt::Formatter) -> std::fmt::Result {
         match &*self {
-            Error::ParseError(ref err) => {
-                write!(f, "Cannot parse parameter: {}", err)
-            }
+            Error::ParseError(ref err) => write!(f, "Cannot parse parameter: {}", err),
             Error::MissingParameters => write!(f, "Missing parameter"),
             Error::WrongPassword => write!(f, "Wrong password"),
             Error::CannotDecryptToken => write!(f, "Cannot decrypt error"),
             Error::Unauthorized => write!(f, "No permission to change the underlying resource"),
-            Error::ArgonLibraryError(_) => {
-                write!(f, "Cannot verifiy password")
-            }
-            Error::DatabaseQueryError(_) => {
-                write!(f, "Cannot update, invalid data")
-            }
+            Error::ArgonLibraryError(_) => write!(f, "Cannot verifiy password"),
+            Error::DatabaseQueryError(_) => write!(f, "Cannot update, invalid data"),
             Error::MigrationError(_) => write!(f, "Cannot migrate data"),
-            Error::ReqwestAPIError(err) => {
-                write!(f, "External API error: {}", err)
-            }
-            Error::MiddlewareReqwestAPIError(err) => {
-                write!(f, "External API error: {}", err)
-            }
-            Error::ClientError(err) => {
-                write!(f, "External Client error: {}", err)
-            }
-            Error::ServerError(err) => {
-                write!(f, "External Server error: {}", err)
-            }
+            Error::ReqwestAPIError(err) => write!(f, "External API error: {}", err),
+            Error::MiddlewareReqwestAPIError(err) => write!(f, "External API error: {}", err),
+            Error::ClientError(err) => write!(f, "External Client error: {}", err),
+            Error::ServerError(err) => write!(f, "External Server error: {}", err),
         }
     }
 }
 
-const DUPLICATE_KEY: u32 = 23505;
-
 impl Reject for Error {}
 impl Reject for APILayerError {}
+
+const DUPLICATE_KEY: u32 = 23505;
 
 #[instrument]
 pub async fn return_error(r: Rejection) -> Result<impl Reply, Rejection> {

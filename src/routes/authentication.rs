f2f79b1f575af947db293f16fa6244a94b677a76
royrustdev <royrustdev@gmail.com>
2023-02-01 10:48:14 +0530

    add tests into auth

--- src/routes/authentication.rs
+++ src/routes/authentication.rs
@@ -97,3 +97,22 @@ pub fn auth() -> impl Filter<Extract = (Session,), Error = warp::Rejection> + Cl
         future::ready(Ok(token))
     })
 }
+
+#[cfg(test)]
+mod authentication_tests {
+    use super::{auth, env, issue_token, AccountId};
+
+    #[tokio::test]
+    async fn post_questions_auth() {
+        env::set_var("PASETO_KEY", "RANDOM WORDS WINTER MACINTOSH PC");
+        let token = issue_token(AccountId(3));
+
+        let filter = auth();
+
+        let res = warp::test::request()
+            .header("Authorization", token)
+            .filter(&filter);
+
+        assert_eq!(res.await.unwrap().account_id, AccountId(3));
+    }
+}

3fc9040c434eb3adca20365a5d2b8dd1db911767
royrustdev <royrustdev@gmail.com>
2023-02-01 10:41:04 +0530

    add tests in config

--- src/config.rs
+++ src/config.rs
@@ -1,8 +1,8 @@
 use clap::Parser;
-use dotenv;
 use std::env;
 
-#[derive(Parser, Debug)]
+/// Q&A web service API
+#[derive(Parser, Debug, PartialEq)]
 #[clap(author, version, about, long_about = None)]
 pub struct Config {
     /// Which errors we want to log (info, warn or error)
@@ -14,8 +14,8 @@ pub struct Config {
     /// Database user
     #[clap(long, default_value = "postgres")]
     pub db_user: String,
-    /// Database password
-    #[clap(long)]
+    /// Database user
+    #[clap(long, default_value = "1234")]
     pub db_password: String,
     /// URL for the postgres database
     #[clap(long, default_value = "localhost")]
@@ -30,28 +30,27 @@ pub struct Config {
 
 impl Config {
     pub fn new() -> Result<Config, handle_errors::Error> {
-        dotenv::dotenv().ok();
         let config = Config::parse();
 
-        if let Err(_) = env::var("BAD_WORDS_API_KEY") {
+        if env::var("BAD_WORDS_API_KEY").is_err() {
             panic!("BadWords API key not set");
         }
 
-        if let Err(_) = env::var("PASETO_KEY") {
+        if env::var("PASETO_KEY").is_err() {
             panic!("PASETO_KEY not set");
         }
 
-        let port = env::var("PORT")
+        let port = std::env::var("PORT")
             .ok()
             .map(|val| val.parse::<u16>())
             .unwrap_or(Ok(config.port))
-            .map_err(|e| handle_errors::Error::ParseError(e))?;
+            .map_err(handle_errors::Error::ParseError)?;
 
-        let db_user = env::var("POSTGRES_USER").unwrap_or(config.db_user.to_owned());
+        let db_user = env::var("POSTGRES_USER").unwrap_or_else(|_| config.db_user.to_owned());
         let db_password = env::var("POSTGRES_PASSWORD").unwrap();
-        let db_host = env::var("POSTGRES_HOST").unwrap_or(config.db_host.to_owned());
-        let db_port = env::var("POSTGRES_PORT").unwrap_or(config.db_port.to_string());
-        let db_name = env::var("POSTGRES_DB").unwrap_or(config.db_name.to_owned());
+        let db_host = env::var("POSTGRES_HOST").unwrap_or_else(|_| config.db_host.to_owned());
+        let db_port = env::var("POSTGRES_PORT").unwrap_or_else(|_| config.db_port.to_string());
+        let db_name = env::var("POSTGRES_DB").unwrap_or_else(|_| config.db_name.to_owned());
 
         Ok(Config {
             log_level: config.log_level,
@@ -61,8 +60,47 @@ impl Config {
             db_host,
             db_port: db_port
                 .parse::<u16>()
-                .map_err(|e| handle_errors::Error::ParseError(e))?,
+                .map_err(handle_errors::Error::ParseError)?,
             db_name,
         })
+    }
+}
+
+#[cfg(test)]
+mod config_tests {
+    use super::*;
+
+    fn set_env() {
+        env::set_var("BAD_WORDS_API_KEY", "API_KEY");
+        env::set_var("PASETO_KEY", "RANDOM WORDS WINTER MACINTOSH PC");
+        env::set_var("POSTGRES_USER", "postgres");
+        env::set_var("POSTGRES_PASSWORD", "1234");
+        env::set_var("POSTGRES_HOST", "localhost");
+        env::set_var("POSTGRES_PORT", "5432");
+        env::set_var("POSTGRES_DB", "warp_rest_api");
+    }
+
+    #[test]
+    fn unset_and_set_api_key() {
+        // ENV VARIABLES ARE NOT SET
+        let result = std::panic::catch_unwind(|| Config::new());
+        assert!(result.is_err());
+
+        // NOW WE SET THEM
+        set_env();
+
+        let expected = Config {
+            log_level: "warn".to_string(),
+            port: 8080,
+            db_user: "postgres".to_string(),
+            db_password: "1234".to_string(),
+            db_host: "localhost".to_string(),
+            db_port: 5432,
+            db_name: "warp_rest_api".to_string(),
+        };
+
+        let config = Config::new().unwrap();
+
+        assert_eq!(config, expected);
     }
 }
